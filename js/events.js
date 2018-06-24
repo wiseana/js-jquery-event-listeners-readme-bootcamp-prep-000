5222ec581e83459134d75311c67d3a0d0bd3e1e1
Anastasia Wise <wise.anastasia@gmail.com>
2018-06-24 23:22:31 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,7 +3,7 @@
 function getIt() {$('p').on('click', function() {alert('Hey!')})}
 function pressIt() {$('form').on('keydown', function(key) {if(key.which==71) {alert('G was pressed');}})}
 function submitIt() {$('form').on('submit', function() {alert('Your form is going to be submitted now.')})}
-function frameIt() {$('img').addClass('tasty')}
+function frameIt() {$('img').on('load', function(){$('img').addClass('tasty')})}
 
 $(document).ready(function(){
 
