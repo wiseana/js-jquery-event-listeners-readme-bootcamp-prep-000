927fb011ddd78041771695eec948d6551c599686
Anastasia Wise <wise.anastasia@gmail.com>
2018-06-24 23:27:01 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,12 @@
 //define functions here
 
+function getIt() {$('p').on('click', function() {alert('Hey!')})}
+function pressIt() {$('form').on('keydown', function(key) {if(key.which==71) {alert('G was pressed');}})}
+function submitIt() {$('form').on('submit', function() {alert('Your form is going to be submitted now.')})}
+function frameIt() {$('img').on('load', function(){$('img').addClass('tasty')})}
+
 $(document).ready(function(){
 
 // call functions here
-
+getIt(); pressIt(); submitIt(); frameIt()
 });
